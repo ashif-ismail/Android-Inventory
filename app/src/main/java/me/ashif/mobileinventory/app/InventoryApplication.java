0c4dfc7a72d95d9ad9bcddc279b9d12605bd2d8f
abdul.ashif <abdul.ashif@techjini.com>
2017-04-11 15:36:46 +0530

    changes to app architecture

--- app/src/main/java/me/ashif/mobileinventory/app/InventoryApplication.java
+++ app/src/main/java/me/ashif/mobileinventory/app/InventoryApplication.java
@@ -1,12 +1,8 @@
 package me.ashif.mobileinventory.app;
 
-import android.app.Activity;
 import android.app.Application;
 
-import me.ashif.mobileinventory.api.InventoryService;
-import me.ashif.mobileinventory.di.components.DaggerInventoryAppComponent;
-import me.ashif.mobileinventory.di.components.InventoryAppComponent;
-import me.ashif.mobileinventory.di.modules.ContextModule;
+import me.ashif.mobileinventory.di.dagger.DaggerInjectorImpl;
 
 /**
  * Created by Ashif on 5/4/17,April,2017
@@ -16,25 +12,20 @@ import me.ashif.mobileinventory.di.modules.ContextModule;
 
 public class InventoryApplication extends Application {
 
-    private InventoryService mInventoryService;
-    private InventoryAppComponent mComponent;
+    private static DaggerInjectorImpl sDaggerInjector;
 
-    public static InventoryApplication get(Activity activity) {
-        return (InventoryApplication) activity.getApplication();
+    public InventoryApplication() {
+        initDagger();
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        mComponent = DaggerInventoryAppComponent.builder()
-                .contextModule(new ContextModule(this))
-                .build();
-
-        mInventoryService = mComponent.getInventoryService();
+    public void initDagger() {
+        getDaggerInjector().init(this);
     }
 
-    public InventoryAppComponent component() {
-        return mComponent;
+    public DaggerInjectorImpl getDaggerInjector() {
+        if (sDaggerInjector == null) {
+            sDaggerInjector = new DaggerInjectorImpl();
+        }
+        return sDaggerInjector;
     }
 }
--- app/src/main/java/me/ashif/mobileinventory/di/components/HomeScreenComponent.java
+++ app/src/main/java/me/ashif/mobileinventory/di/components/HomeScreenComponent.java
@@ -2,16 +2,16 @@ package me.ashif.mobileinventory.di.components;
 
 import dagger.Component;
 import me.ashif.mobileinventory.di.modules.HomeScreenModule;
+import me.ashif.mobileinventory.di.modules.InventoryAppModule;
 import me.ashif.mobileinventory.di.scopes.PerActivity;
 import me.ashif.mobileinventory.ui.homescreen.HomeScreenActivity;
-import me.ashif.mobileinventory.ui.homescreen.HomeScreenAdapter;
 
 /**
  * Created by Ashif on 5/4/17,April,2017
  * TechJini Solutions
  * Banglore,India
  */
-@Component(modules = HomeScreenModule.class)
+@Component(modules = HomeScreenModule.class, dependencies = InventoryAppModule.class)
 @PerActivity
 public interface HomeScreenComponent {
     void inject(HomeScreenActivity homeScreenActivity);
